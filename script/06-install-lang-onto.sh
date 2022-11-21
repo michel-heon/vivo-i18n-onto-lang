ed387d0909dbe1e9e032f9e1790c2dd2f2bff5a9
michel-heon <heon.michel@uqam.ca>
2022-11-21 08:01:52 +0000

    Correction of README_* files to replace 'display' by 'interface-i18n'

--- script/06-install-lang-onto.sh
+++ script/06-install-lang-onto.sh
@@ -51,7 +51,7 @@ function process_file {
 Please note that although usage of property files for translation of UI labels is supported at the moment,
 it is deprecated and not recommended. Please, consider using ontology instead of property file located at:
 Source code: [VIVO project]$LANG_REPO/$UI_ONTO_FN
-Deployment: [VIVO home]/rdf/i18n/$REGION/display/firsttime/$UI_ONTO_FN
+Deployment: [VIVO home]/rdf/i18n/$REGION/display/interface-i18n/$UI_ONTO_FN
 
 However, if you decide to use property files, please create and post the file in the same
 directory as this Readme file.
